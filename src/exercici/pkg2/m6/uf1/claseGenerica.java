538d0c02cd6ce404541b4a514653126d420902be
davidblancolopez <daviddbl94@gmail.com>
2017-02-02 20:10:37 +0100

    CAMBIOS

---
+++ src/exercici/pkg2/m6/uf1/claseGenerica.java
@@ -0,0 +1,18 @@
+//import org.w3c.dom.Document;
+//import org.w3c.dom.Element;
+
+
+package exercici.pkg2.m6.uf1;
+
+/**
+ *
+ * @author ALUMNEDAM
+ */
+public abstract class claseGenerica <T>{
+    
+    public claseGenerica(String rutaDocument){
+        
+    }
+    
+    
+}
