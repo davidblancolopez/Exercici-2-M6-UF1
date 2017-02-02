d5265683bce34370a665f0bf81297e3951969194
davidblancolopez <daviddbl94@gmail.com>
2017-02-02 19:31:29 +0100

    CAMBIOS

---
+++ src/exercici/pkg2/m6/uf1/Exercici2M6UF1.java
@@ -0,0 +1,14 @@
+
+package exercici.pkg2.m6.uf1;
+
+
+public class Exercici2M6UF1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
