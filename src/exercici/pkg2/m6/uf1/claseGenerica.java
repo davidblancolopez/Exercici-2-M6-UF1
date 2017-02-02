47cbd5e6e47dc5ea0b775e9a754cbb86d912db44
davidblancolopez <daviddbl94@gmail.com>
2017-02-02 20:18:29 +0100

    CAMBIOS

--- src/exercici/pkg2/m6/uf1/claseGenerica.java
+++ src/exercici/pkg2/m6/uf1/claseGenerica.java
@@ -4,15 +4,33 @@
 
 package exercici.pkg2.m6.uf1;
 
+import java.io.File;
+import java.io.IOException;
+import jdk.internal.org.xml.sax.SAXException;
+import org.w3c.dom.Document;
+
 /**
  *
  * @author ALUMNEDAM
  */
 public abstract class claseGenerica <T>{
     
+    protected File fitxerXML; 
+    
     public claseGenerica(String rutaDocument){
-        
+        this.fitxerXML = new File(rutaDocument);
     }
+    
+    public Document carregarFitxerXML() throws SAXException, IOException, ParserConfig;
+    
+    public void deasrDoc(Document doc) throws TransformerConfigrationException;
+    
+    public abstract void afegirObjecte(T object);
+    
+    public abstract T cercarPerCodi(String codi);
+    
+    public abstract List<T> ;
+    
     
     
 }
