bdd6bcb5de6759522b17242be4208415e4b9b46a
davidblancolopez <daviddbl94@gmail.com>
2017-02-02 20:34:35 +0100

    CAMBIOS

--- src/exercici/pkg2/m6/uf1/gestioProducte.java
+++ src/exercici/pkg2/m6/uf1/gestioProducte.java
@@ -5,10 +5,57 @@
  */
 package exercici.pkg2.m6.uf1;
 
+import java.util.List;
+
 /**
  *
  * @author ALUMNEDAM
  */
 public class gestioProducte extends gestioXML<T>{
+
+    @Override
+    public void afegirObjecte(T object) {
+        try{
+            
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public T cercarPerCodi(String codi) {
+        try{
+            
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public List<T> retornarObjectes() {
+        try{
+            
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void modificarObjecte(T object) {
+        try{
+            
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void eliminarObjecte(T object) {
+        try{
+            
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
     
 }
